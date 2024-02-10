d46af40e96a154d7631b02ce5a8bd9588e380aef
jOsuna85 <131856355+jOsuna85@users.noreply.github.com>
2024-02-09 19:33:14 -0700

    Enviar Data a Firebase

--- app/src/main/java/com/josuna85/holafirebase/MainActivity.kt
+++ app/src/main/java/com/josuna85/holafirebase/MainActivity.kt
@@ -3,6 +3,8 @@ package com.josuna85.holafirebase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -14,23 +16,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val database = Firebase.database.reference
+        val database = Firebase.database.reference //busco base de datos
+        //especifico la ruta y el dato en esto caso "data"
+        val dataRef = database.child("hola_firebase").child("data")
 
         val listener = object : ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
                 val data = snapshot.getValue(String::class.java)
                 findViewById<TextView>(R.id.tvData).text = "Firebase remote: $data"
             }
-
             override fun onCancelled(error: DatabaseError) {
 
             }
-
         }
-
-        val dataRef = database.child("hola_firebase").child("data")
-
+        //cargo el listener con los valores del dato en "data"
         dataRef.addValueEventListener(listener)
+
+        findViewById<MaterialButton>(R.id.btnSend).setOnClickListener {
+            val data = findViewById<TextInputEditText>(R.id.etData).text.toString()
+            dataRef.setValue(data)
+        }
 
     }
 }
